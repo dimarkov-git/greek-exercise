1ca76fb354152edcb1a60093e0918e7e2b1d4c27
d.markov <d.markov@space307.com>
2025-09-18 15:17:52 +0300

    [main] Fix import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 
 import path from 'node:path'
+import process from 'node:process'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
