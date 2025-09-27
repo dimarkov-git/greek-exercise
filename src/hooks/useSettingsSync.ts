0debe48e37d7d01f1705673c0d06e3c9f7f39654
d.markov <d.markov@space307.com>
2025-09-27 23:27:40 +0300

    [main] Fix dark theme

--- src/hooks/useSettingsSync.ts
+++ src/hooks/useSettingsSync.ts
@@ -7,6 +7,12 @@ export function setDocumentTheme(theme: string) {
 	}
 
 	document.documentElement.setAttribute('data-theme', theme)
+
+	if (theme === 'dark') {
+		document.documentElement.classList.add('dark')
+	} else {
+		document.documentElement.classList.remove('dark')
+	}
 }
 
 export function setDocumentLanguage(language: string) {
